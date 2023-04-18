f63d9b02db0e3a78cb8844c1b416f22afb5ede30
anjususan93 <127041551+anjususan93@users.noreply.github.com>
2023-04-18 17:01:00 -0400

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,19 @@
+console.log("Creating Output")
+
+let title= "<h1>W3Schools</h1>"
+let ans="<p>W3Schools is a web developer information website, with tutorials\n and references relating to web development topics such as HTML, CSS,\n JavaScript and PHP.</p>"
+
+let text = "W3Schools";
+let result = text.link("http://www.w3schools.com/");
+
+
+let img ="http://www.w3schools.com/images/w3schools.png"
+var x = document.getElementById("img");
+
+
+document.write (title)
+document.write (ans)
+document.write (result)
+document.write (x)
+
+
